a2d2db065f83058053ae1ee14779ef65e6ed613e
Schmoozerd <schmoozerd@scriptdev2.com>
2013-01-19 22:25:19 +0100

    Fix MOT Guid

--- src/game/GameObject.cpp
+++ src/game/GameObject.cpp
@@ -145,7 +145,10 @@ bool GameObject::Create(uint32 guidlow, uint32 name_id, Map* map, uint32 phaseMa
         return false;
     }
 
-    Object::_Create(guidlow, goinfo->id, goinfo->type == GAMEOBJECT_TYPE_MO_TRANSPORT ? HIGHGUID_MO_TRANSPORT : HIGHGUID_GAMEOBJECT);
+    if (goinfo->type == GAMEOBJECT_TYPE_MO_TRANSPORT)
+        Object::_Create(goinfo->id, 0, HIGHGUID_MO_TRANSPORT);
+    else
+        Object::_Create(guidlow, goinfo->id, HIGHGUID_GAMEOBJECT);
 
     m_goInfo = goinfo;
 
